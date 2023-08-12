06a0ec6ac6c3bffd3499c48cd6e4eeb102abb284
Glitchtest <141680927+Glitchtest51@users.noreply.github.com>
2023-08-12 16:52:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const textElement = document.getElementById("Glitchtest.site");
 
-const baseURL = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}`;
-console.log(baseURL);
-
 const typeText = (text, delay) => {
   let i = 0;
   const interval = setInterval(() => {
