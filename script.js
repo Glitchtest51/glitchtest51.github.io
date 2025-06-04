ad4c96b20f58f02f419c855155dad003deed2809
sovimuffins <git.sovimuffins.2zp7z@simplelogin.fr>
2025-06-04 09:41:15 -0400

    MAYBE it won't find anything wrong now

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ typingSound.preload = "auto";
 
 const typeText = (text, delay) => {
   let i = 0;
+  if (!textElement) return;
   const interval = setInterval(() => {
     if (i === text.length) {
       clearInterval(interval);
     }
     const subText = text.substring(0, i + 1);
-    if (textElement) {
-      textElement.textContent = subText;
-      try {
-        typingSound.currentTime = 0;
-        typingSound.play();
-      } catch (e) {
-        console.error('Error playing typing sound:', e);
-      }
+    textElement.textContent = subText;
+    try {
+      typingSound.currentTime = 0;
+      typingSound.play();
+    } catch (e) {
+      console.error('Error playing typing sound:', e);
     }
     i++;
   }, delay);
